424f6538ccd0314928e6041c607973be27472c54
Conor Micallef-Green <35455234+Silvertooth98@users.noreply.github.com>
2025-02-15 15:53:38 +0000

    Removed CMGGameplayFramework plugin (cleanup)

--- Plugins/CMGGameplayFramework/Source/CMGGameplayFramework/Private/Interaction/CMGInteractInterface.cpp
+++
@@ -1,5 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Interaction/CMGInteractInterface.h"
-
-// Add default functionality here for any ICMGInteractInterface functions that are not pure virtual.
